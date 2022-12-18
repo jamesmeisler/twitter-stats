d2da7715c0fe2595320eb57743602826e78f7f1f
James Meisler <jgonzalesmeisler@avtex.com>
2022-12-18 13:55:02 -0600

    Initial commit

---
+++ apps/twitter-stats/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'twitter-stats-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'twitter-stats';
+}
