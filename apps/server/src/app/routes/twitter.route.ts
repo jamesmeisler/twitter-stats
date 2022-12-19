5eebbdf670b56fc1ba086b6476d7828bd02383e0
James Meisler <jgonzalesmeisler@avtex.com>
2022-12-19 09:36:17 -0600

    updates to twitter routing

--- apps/server/src/app/routes/twitter.route.ts
+++ apps/server/src/app/routes/twitter.route.ts
@@ -12,4 +12,21 @@ router.get('/', async (req, res) => {
     res.json(userResult.data);
 });
 
+router.get('/:user/timeline', async (req, res) => {
+    const userResult = await twitterClient.v2.userByUsername('holycity15', {
+        "user.fields": ['created_at', 'description', 'username', 'verified']
+    });
+    const id = userResult.data.id
+    const userTimelinePaginator = await twitterClient.v2.userTimeline(id, {
+        'tweet.fields': ['created_at', 'public_metrics', 'geo', 'in_reply_to_user_id']
+    });
+
+    const tweets = await userTimelinePaginator.fetchLast(100);
+    res.json({
+        user: userResult.data,
+        tweets: tweets.data
+    });
+
+});
+
 export default router;
